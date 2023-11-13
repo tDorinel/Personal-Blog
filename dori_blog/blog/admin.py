d4c63926e10b702a776c973758a6cf810b4c612a
tDorinel <todoran.dorinel@gmail.com>
2023-11-13 22:59:08 +0200

    New Changes. Comment section added using if and for.

--- dori_blog/blog/admin.py
+++ dori_blog/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
 admin.site.site_header = "Dori's Blog Control Panel"
+admin.site.register(Comment)
--- dori_blog/blog/models.py
+++ dori_blog/blog/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.post_title + ' - ' + str(self.post_author.get_full_name())
+    
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post,related_name="comments",on_delete=models.CASCADE)
+    comment_name = models.CharField(max_length=255)
+    comment_body = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.comment_post) + ' - ' + str(self.comment_name)
+       
